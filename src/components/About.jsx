06f5af513b3cc551e21e0133bebc4af3255154c5
JohnJosueVendanillo <104766530+JohnJosueVendanillo@users.noreply.github.com>
2022-11-15 14:30:01 +0800

    finished portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl py-6">
+          Dolore pariatur sunt ad aliquip eiusmod in esse aliqua eu nisi
+          consequat. Fugiat ea duis ad sit elit incididunt est ullamco irure
+          labore ea quis reprehenderit minim. Exercitation esse sunt anim non.
+          Sit sint eiusmod Lorem fugiat ad enim ullamco fugiat irure eu dolore
+          sunt reprehenderit. Minim excepteur nisi nisi minim eiusmod cupidatat
+          ad aliquip in nostrud. Eu irure veniam in magna consequat. Sunt nulla
+          laboris et anim non ullamco.
+        </p>
+        <br />
+        <p className="text-xl">
+          Consequat id sit pariatur veniam esse ipsum enim velit voluptate elit.
+          Minim labore proident in cillum tempor. Culpa labore et consequat elit
+          deserunt laboris enim incididunt. Incididunt sint enim pariatur
+          laborum occaecat proident. Lorem amet eiusmod eiusmod cupidatat
+          officia do enim id eiusmod est est officia.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
